2b104fb0ab6e4d98f645ae57a10ed03b2e00130c
丁嘉一 <jiayiding1997@gmail.com>
2022-05-06 11:47:09 +0800

    ADD note on initializer_list

--- src/vector.h
+++ src/vector.h
@@ -1,6 +1,9 @@
 #ifndef TINYSTL_VECTOR_H_
 #define TINYSTL_VECTOR_H_
 
+
+//An object of type std::initializer_list<T> is a lightweight proxy object 
+//that provides access to an array of objects of type const T.
 #include<initializer_list>
 
 #include "memory.h"
